8c07038f6da57487bac208a5aa85a6664261584c
Nicolas Thery <nthery@gmail.com>
2021-02-14 20:07:40 +0100

    initial version
    The core supports an arbitrary number of input files but the CLI imposes
    two input files for the time being.
    
    There are no flags yet to select what field is the key and what the
    separator is.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nthery/gojoin
+
+go 1.15
---
+++ gojoin_test.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+)
+
+func TestJoinTwoInputs(t *testing.T) {
+	tests := []struct {
+		intent, left, right, want string
+	}{
+		{"empty inputs", "", "", ""},
+		{"empty lines", "\n", "\n", ""},
+		{"first lines match", "1 a", "1 b", "1 a b\n"},
+		{"no match", "1 a", "2 b", ""},
+		{"left[1] matches right[0]", "1 a\n2 b", "2 c", "2 b c\n"},
+		{"left[0] matches right[1]", "2 a", "1 b\n2 c", "2 a c\n"},
+		{"first lines empty, second ones match", "\n1 a", "\n1 b", "1 a b\n"},
+		{"matching lines with single word", "1", "1", "1\n"},
+		{"all lines match", "1 a\n2 b", "1 c\n2 d", "1 a c\n2 b d\n"},
+		{"last line has end-of-line", "1 a\n", "1 b\n", "1 a b\n"},
+		{"inputs with differing # of lines", "1 a", "1 b\n2 c", "1 a b\n"},
+	}
+	for _, test := range tests {
+		var inputs [2]input
+		inputs[0].init(strings.NewReader(test.left), "left")
+		inputs[1].init(strings.NewReader(test.right), "right")
+		var output bytes.Buffer
+		err := join(inputs[:], &output)
+		if err != nil {
+			t.Errorf("%s: unexpected error: %v", test.intent, err)
+		}
+		got := output.String()
+		if got != test.want {
+			t.Errorf("%s: want: %q got: %q", test.intent, test.want, got)
+		}
+	}
+}
+
+func TestJoinThreeInputs(t *testing.T) {
+	// Most cases are exercised in TestJoinTwoInputs().
+	tests := []struct {
+		intent, left, mid, right, want string
+	}{
+		{"all inputs match", "1 a", "1 b", "1 c", "1 a b c\n"},
+		{"only some input match", "1 a", "2 b", "1 c", ""},
+	}
+	for _, test := range tests {
+		var inputs [3]input
+		inputs[0].init(strings.NewReader(test.left), "left")
+		inputs[1].init(strings.NewReader(test.mid), "mid")
+		inputs[2].init(strings.NewReader(test.right), "right")
+		var output bytes.Buffer
+		err := join(inputs[:], &output)
+		if err != nil {
+			t.Errorf("%s: unexpected error: %v", test.intent, err)
+		}
+		got := output.String()
+		if got != test.want {
+			t.Errorf("%s: want: %q got: %q", test.intent, test.want, got)
+		}
+	}
+}
