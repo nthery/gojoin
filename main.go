7716152c895373f1c1f1caa494929e3109cb8822
Nicolas Thery <nthery@gmail.com>
2021-02-19 09:09:53 +0100

    cli supports arbitrary separator

--- main.go
+++ main.go
@@ -2,6 +2,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
@@ -11,8 +12,17 @@ import (
 // Number of files expected on command-line.
 const nfiles = 2
 
+var sep = flag.String("t", " ", "field separator")
+
 func main() {
-	args := os.Args[1:]
+	flag.Parse()
+
+	if len(*sep) == 0 {
+		fmt.Fprintln(os.Stderr, "gojoin: separator must not be empty")
+		os.Exit(1)
+	}
+
+	args := flag.Args()
 	if len(args) != 2 {
 		usage()
 		os.Exit(1)
@@ -29,7 +39,7 @@ func main() {
 		inputs[i] = join.NewInput(f, args[i])
 	}
 
-	err := join.Join(inputs[:], " ", os.Stdout)
+	err := join.Join(inputs[:], *sep, os.Stdout)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "gojoin: error while joining files: %v\n", err)
 		os.Exit(1)
