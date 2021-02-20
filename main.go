0c700e03bbab7d2ed78752b47fd73822dfdf0e13
Nicolas Thery <nthery@gmail.com>
2021-02-20 15:40:00 +0100

    use log to report errors

--- main.go
+++ main.go
@@ -4,33 +4,33 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/nthery/gojoin/join"
 )
 
-var sep = flag.String("t", " ", "field separator")
-
 func main() {
+	log.SetPrefix("gojoin: ")
+	log.SetFlags(0)
+
+	sep := flag.String("t", " ", "field separator")
 	flag.Parse()
 
 	if len(*sep) == 0 {
-		fmt.Fprintln(os.Stderr, "gojoin: separator must not be empty")
-		os.Exit(1)
+		log.Fatal("separator must not be empty")
 	}
 
 	args := flag.Args()
 	if len(args) < 2 {
 		usage()
-		os.Exit(1)
 	}
 
 	inputs := make([]join.Input, 0, len(args))
 	for _, path := range args {
 		f, err := os.OpenFile(path, os.O_RDONLY, 0)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "gojoin: cannot open %v: %v\n", path, err)
-			os.Exit(1)
+			log.Fatalf("error: %v", err)
 		}
 		defer f.Close()
 		inputs = append(inputs, join.NewInput(f, path))
@@ -38,11 +38,11 @@ func main() {
 
 	err := join.Join(inputs, *sep, os.Stdout)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "gojoin: error while joining files: %v\n", err)
-		os.Exit(1)
+		log.Fatalf("error while joining files: %v", err)
 	}
 }
 
 func usage() {
 	fmt.Fprintln(os.Stderr, "usage: gojoin [-t sep] file...")
+	os.Exit(1)
 }
